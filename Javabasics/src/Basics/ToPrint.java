c2b94baa92bc36d1c4fae4d63e9d17425b567d76
abhis <abhis@Abhijith>
2023-03-13 12:43:37 +0530

    Added bye..... msg

--- Javabasics/src/Basics/ToPrint.java
+++ Javabasics/src/Basics/ToPrint.java
@@ -5,6 +5,6 @@ public class ToPrint {
 	public static void main(String[] args) {
 		System.out.println("Hello...");
 		System.out.println("Welcome...");
-		System.out.println("Bye");
+		System.out.println("Bye .......");
 	}
 }
